28b88c95537330d3401d7b17b3948e02755e5cd7
Griffin Cloney <griffboy268@gmail.com>
2018-11-15 08:46:26 -1000

    Initial Commit

---
+++ swipey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swipey
+//
+//  Created by Griffin Cloney on 11/5/18.
+//  Copyright © 2018 Griffin Cloney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
